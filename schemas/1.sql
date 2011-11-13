f6671f7f5a2c17e15d93b5c89d6d65120ff914d1
Dag Hovland <hovlanddag@gmail.com>
2011-11-13 14:49:19 +0100

    La til roller i schema/1.sql

--- schemas/1.sql
+++ schemas/1.sql
@@ -1,2 +1,10 @@
-CREATE TABLE versions (number UNSIGNED INT NOT NULL, name VARCHAR(100), PRIMARY KEY(name)) DEFAULT CHARSET = utf8;
-INSERT INTO versions VALUES (1, 'db_schema');
+ALTER TABLE roller ADD UNIQUE INDEX(navn);
+INSERT IGNORE INTO roller (navn) VALUES ('kasserer');
+INSERT IGNORE INTO roller (navn) VALUES ('bruker');
+INSERT IGNORE INTO roller (navn) VALUES ('admin');
+INSERT IGNORE INTO roller (navn) VALUES ('lager');
+INSERT IGNORE INTO roller (navn) VALUES ('kunde');
+       	      
+
+CREATE TABLE IF NOT EXISTS versions (number INT UNSIGNED NOT NULL, name VARCHAR(100), PRIMARY KEY(name)) DEFAULT CHARSET = utf8;
+REPLACE INTO versions VALUES (1, 'db_schema');
