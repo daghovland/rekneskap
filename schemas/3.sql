003d71286fff094af77290d43a8ba368185fc187
Dag Hovland <hovlanddag@gmail.com>
2011-11-15 00:09:06 +0100

    Error in 3.sql (same name on both triggers.)

--- schemas/3.sql
+++ schemas/3.sql
@@ -1,4 +1,4 @@
 /* For keeping track of keys for forgotten passwords */
-CREATE TRIGGER tmp_key_update BEFORE INSERT ON selgere FOR EACH ROW SET NEW.tmp_key_created = NOW();
+CREATE TRIGGER tmp_key_insert BEFORE INSERT ON selgere FOR EACH ROW SET NEW.tmp_key_created = NOW();
 CREATE TRIGGER tmp_key_update BEFORE UPDATE ON selgere FOR EACH ROW SET NEW.tmp_key_created = NOW();
 REPLACE INTO versions VALUES (3, 'db_schema');
