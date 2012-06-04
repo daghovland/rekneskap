b99f4016a1c6dd2fbc55c4522e97f23124999fc6
Dag Hovland <hovlanddag@gmail.com>
2012-06-04 21:58:37 +0200

    Oppdaterte tingingar tabellen med mere info

---
+++ View/Tingingar/start_xmlrpc_server.ctp
@@ -0,0 +1,9 @@
+<div class="tingingar view">
+<h2><?php  echo __('Tinging');?></h2>
+<div class="actions">
+	<h3><?php echo __('Actions'); ?></h3>
+	<ul>
+		<li><?php echo $this->Html->link(__('List Tingingar'), array('action' => 'index')); ?> </li>
+		<li><?php echo $this->Html->link(__('New Tinging'), array('action' => 'add')); ?> </li>
+	</ul>
+</div>
--- schemas/9.sql
+++ schemas/9.sql
@@ -1,14 +1,15 @@
 #Lager en tabell for "tingingar", dvs. tingingar som ikkje har vorte til verkerlege sal endå
 CREATE TABLE  IF NOT EXISTS `tingingar` (
        `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+       `ubercart_ordre_id` INT(10) UNSIGNED DEFAULT NULL,
        `tinga` datetime NOT NULL,
        `kunde_id` INT(10) UNSIGNED,
        `total` INT(10) UNSIGNED,
        `frakt` INT(10) UNSIGNED,
-       `varetekst` VARCHAR(300) NOT NULL,
-       `kundetekst` VARCHAR(300) NOT NULL,
        `modified` datetime DEFAULT NULL,
        `created` datetime DEFAULT NULL,
+        KEY `kunde_id` (`kunde_id`), 
+        CONSTRAINT `tinging_kunde` FOREIGN KEY (`kunde_id`) REFERENCES `kunder` (`nummer`),
        PRIMARY KEY (`id`)
   )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
